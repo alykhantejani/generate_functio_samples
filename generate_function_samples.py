7fa28f23cdac6fcb97502bf189044abe2287bc1d
Alykhan Tejani <alykhan.tejani@gmail.com>
2015-11-08 17:05:51 +0000

    added support for 3d plots

--- generate_function_samples.py
+++ generate_function_samples.py
@@ -1,33 +1,61 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import random
 import numexpr as ne
 import argparse
+import json
 
-def enable_smart_axes(subplot):
-	# set axis lines to be smart
-	subplot.spines['left'].set_position('zero')
-	subplot.spines['right'].set_color('none')
-	subplot.spines['bottom'].set_position('zero')
-	subplot.spines['top'].set_color('none')
-	subplot.spines['left'].set_smart_bounds(True)
-	subplot.spines['bottom'].set_smart_bounds(True)
-	subplot.xaxis.set_ticks_position('bottom')
-	subplot.yaxis.set_ticks_position('left')
+def plot_2d(samples_ranges, sample_values, true_values, true_values_ranges):
+	fig = plt.figure()
+	
+	keys = samples_ranges.keys()
 
+	subplot = fig.add_subplot(111)
+
+	subplot.set_xlabel(keys[0])
+
+	subplot.set_xlim(min(samples_ranges[keys[0]]), max(samples_ranges[keys[0]]))
+	subplot.set_ylim([min(samples), max(samples)])
+
+	subplot.plot(samples_ranges[keys[0]], samples, 'bo', clip_on = False)
+
+	if true_values is  not None:
+			subplot.plot(true_values_ranges[true_values_ranges.keys()[0]], true_values, 'r-', alpha = 0.2)
+
+	return subplot
+
+def plot_3d(samples_ranges, sample_values, true_values, true_values_ranges):
+	fig = plt.figure()
+	subplot = fig.add_subplot(111, projection = '3d')
+	
+	keys = samples_ranges.keys()
+
+	subplot.set_xlabel(keys[0])
+	subplot.set_ylabel(keys[1])
+
+	subplot.set_xlim(min(samples_ranges[keys[0]]), max(samples_ranges[keys[0]]))
+	subplot.set_ylim(min(samples_ranges[keys[0]]), max(samples_ranges[keys[0]]))
+	subplot.set_zlim([min(samples), max(samples)])
+
+	subplot.plot(samples_ranges[keys[0]], samples_ranges[keys[1]], samples, 'bo', clip_on = False)
+
+	if true_values is  not None:
+		subplot.plot(true_values_ranges[true_values_ranges.keys()[0]], true_values_ranges[true_values_ranges.keys()[1]], true_values, 'r-', alpha = 0.2)
+
+	return subplot
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--function', type = str, required = True, help = 'function to plot expressed in terms of variable x')
+parser.add_argument('--function', type = str, required = True, help = 'function to plot expressed in terms of variables in variable map')
+parser.add_argument('--variable_ranges', type = json.loads, required = True, help = 'range of variables in function i.e. \'{ "x1" : [-1, 1], "x2" : [-2, 2] }\'')
+parser.add_argument('--function_output_name', type = str, default = 'y', help = 'name to display for output of function')
 parser.add_argument('--plot_out_file', type = str, default = None, help = 'File to save plot to')
-parser.add_argument('--samples_out_file', type = str, default = None, help = 'File to save samples (x,y) to (.npy)')
+parser.add_argument('--samples_out_file', type = str, default = None, help = 'File to save samples (y,x) to (.npy)')
 parser.add_argument('--jitter', type = float, default = 0.5, help = 'jitter range +/- [jitter]')
 parser.add_argument('--num_samples', type = int, default = 15, help = 'number of samples to generate')
-parser.add_argument('--xrange', nargs = 2, type = float, default = [-1, 1], help = 'x-axis range in format min max')
 parser.add_argument('--draw_true_function', action='store_true')
-parser.add_argument('--x_label', type = str, default = 'x')
-parser.add_argument('--y_label', type = str, default = 'y')
 parser.add_argument('--title', type = str, default = '')
-parser.add_argument('--enable_smart_axes', action = 'store_true', help = 'Make axes lines cross (0,0)')
+parser.add_argument('--display', action = 'store_true', help = 'Display live interactive visualization')
 
 args = parser.parse_args()
 
@@ -35,50 +63,54 @@ jitter = args.jitter
 plot_out_file = args.plot_out_file
 samples_out_file = args.samples_out_file
 num_samples = args.num_samples
+variable_ranges = args.variable_ranges
 
-x_min = float(args.xrange[0])
-x_max = float(args.xrange[1])
+assert len(variable_ranges) > 0 and len(variable_ranges) <= 2, 'Can only plot 2D or 3D graphs'
+
+samples_eval_ranges = {}
+for key in variable_ranges:
+	samples_eval_ranges[key] = np.linspace(variable_ranges[key][0], variable_ranges[key][1], num_samples)
 
 f = args.function
-
 #plot initial data
-x = np.linspace(x_min, x_max, num_samples)
-samples = ne.evaluate(f, local_dict = {'x': x})
+samples = ne.evaluate(f, local_dict = samples_eval_ranges)
 
 for i in xrange(0, len(samples)):
 	samples[i] = samples[i] + random.uniform(-jitter, jitter) 
 
-margin = 0.25
+true_values = None
+true_eval_ranges = None
+if args.draw_true_function:
+	true_eval_ranges = {}
 
-fig = plt.figure()
-subplot = fig.add_subplot(111)
+	for key in variable_ranges:
+		true_eval_ranges[key] = np.linspace(variable_ranges[key][0], variable_ranges[key][1], 100)
 
-subplot.set_xlabel(args.x_label)
-subplot.set_ylabel(args.y_label)
+	true_values = ne.evaluate(f, local_dict = true_eval_ranges)
+
+subplot = None
+if len(variable_ranges) == 1:
+	subplot = plot_2d(samples_eval_ranges, samples, true_values, true_eval_ranges)
+	subplot.set_ylabel(args.function_output_name)
+else:
+	subplot = plot_3d(samples_eval_ranges, samples, true_values, true_eval_ranges)
+	subplot.set_zlabel(args.function_output_name)
+
 subplot.set_title(args.title)
-
-subplot.set_xlim([x_min, x_max])
-subplot.set_ylim([min(samples), max(samples)])
-
-if args.enable_smart_axes:
-	enable_smart_axes(subplot)
 
 subplot.grid()
 
-subplot.plot(x, samples, 'bo', clip_on = False)
-
-if args.draw_true_function:
-	x_all = np.linspace(x_min, x_max, 100)
-	y = ne.evaluate(f, local_dict = {'x' : x_all})
-	subplot.plot(x_all, y, 'r-', alpha = 0.2)
-
 if samples_out_file:
 	print('saving samples to ' + samples_out_file)
-	paired_samples = zip(x,samples)
-	np.save(samples_out_file, paired_samples)
+	out = {'out_values': samples}
+	for k in samples_eval_ranges:
+		out[k] = samples_eval_ranges[k]
+
+	np.save(samples_out_file, out)
 
 if plot_out_file:
 	print('saving plot to ' + plot_out_file)
 	plt.savefig(plot_out_file)
-else:
+
+if args.display:
 	plt.show()
